77e2d8f67569706f2b6957b5456f35cf2c7ed2b3
Ambrose Montenegro-Mackenzie <brosegm@gmail.com>
2022-12-04 14:17:49 -0500

    added coding challenge for today, 04-12-2022

---
+++ cc04-12-2022/ispalindrome.js
@@ -0,0 +1,7 @@
+/*
+Write a function that checks if a given string (case insensitive) is a palindrome.
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
+ */
+function isPalindrome(x) {
+  
+}
