60bd13de7f3c6fcdc1a2c00ea36fe2fb5ffa7b4d
Ambrose Montenegro-Mackenzie <brosegm@gmail.com>
2022-12-06 15:59:42 -0500

    commit

--- cc04-12-2022/ispalindrome.js
+++ cc04-12-2022/ispalindrome.js
@@ -3,5 +3,7 @@ Write a function that checks if a given string (case insensitive) is a palindrom
 https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
  */
 function isPalindrome(x) {
-  
+    a = x.toLowerCase().split('').join('')
+    b = x.toLowerCase().split('').reverse().join('')
+    return a==b
 }
