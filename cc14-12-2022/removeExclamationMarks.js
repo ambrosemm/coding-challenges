76ac80e1adb58f42a9f1e7db380e1d237c0ae5f5
Ambrose Montenegro-Mackenzie <brosegm@gmail.com>
2023-01-16 13:46:33 -0500

    Added coding challenge for 26-1-2023

--- cc14-12-2022/removeExclamationMarks.js
+++ cc14-12-2022/removeExclamationMarks.js
@@ -4,5 +4,5 @@ Write function RemoveExclamationMarks which removes all exclamation marks from a
 https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
 */
 function removeExclamationMarks(s) {
-    return s.replace('!', '');
-}
+    return s.split('!').join('');
+  }
