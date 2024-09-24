285f089f31d45b206295709b73dc32cdc6768e87
Ambrose Montenegro-Mackenzie <brosegm@gmail.com>
2024-09-24 14:43:08 -0400

    Added another coding challenge

---
+++ cc24-09-2024/1000x.py
@@ -0,0 +1,8 @@
+"""
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.
+
+https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/python
+"""
+websites = ["codewars"]*1000
